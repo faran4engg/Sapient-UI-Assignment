c3facac43911f3fe14c720291eb1864092e67803
Faran Shaikh <faran4engg@gmail.com>
2018-07-13 01:27:24 +0530

    folder details bootstrap code - > faran-code plain css awesome code - > sapient

--- sapient/src/app/app.module.ts
+++ sapient/src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HomeComponent,
+    NavComponent,
+    ProfileComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
